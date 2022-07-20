cbee563565e8d2dd38526f8c7e1823d96f38882b
Abraham <abrahamsmith1998@gmail.com>
2022-07-20 02:18:53 -0400

    classes on time-blocks are changing correctly based on current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,88 +1,80 @@
 var today = moment();
 $('#currentDay').text(today.format("MMM Do, YYYY"));
 
-$('#9am').val(localStorage.getItem('9am task'));
-$('#10am').val(localStorage.getItem('10am task'));
-$('#11am').val(localStorage.getItem('11am task'));
-$('#12pm').val(localStorage.getItem('12pm task'));
-$('#1pm').val(localStorage.getItem('1pm task'));
-$('#2pm').val(localStorage.getItem('2pm task'));
-$('#3pm').val(localStorage.getItem('3pm task'));
-$('#4pm').val(localStorage.getItem('4pm task'));
-$('#5pm').val(localStorage.getItem('5pm task'));
+$('#09').val(localStorage.getItem('9am task'));
+$('#10').val(localStorage.getItem('10am task'));
+$('#11').val(localStorage.getItem('11am task'));
+$('#12').val(localStorage.getItem('12pm task'));
+$('#13').val(localStorage.getItem('1pm task'));
+$('#14').val(localStorage.getItem('2pm task'));
+$('#15').val(localStorage.getItem('3pm task'));
+$('#16').val(localStorage.getItem('4pm task'));
+$('#17').val(localStorage.getItem('5pm task'));
 
-function present(){
-    if (moment().isSame(moment())){
+const ids = $('#form').children();
+console.log(typeof ids, ids);
+ids.toArray().forEach(checkTime);
+
+function checkTime(id) {
+    console.log(id);
+    var timeValue = moment($(id).attr('id'), ['HH']).format('HH');
+    var currentTime = moment().format('HH');
+
+    console.log(typeof currentTime, currentTime);
+    console.log(typeof timeValue, timeValue);
+    if (timeValue == moment().format('hh')){
+        $(id).addClass('present');
+    } else if (timeValue < currentTime) {
         console.log('hello');
-        $('#9am').addClass('present');
+        $(id).addClass('past');
     } else {
-        console.log('goodbye');
-    };
-}
-
-// var checkTime = function () {
-//     var time = $(".time").value;
-
-//     var hour = moment(time, "LT");
-//     console.log(hour);
-
-//     //remove any old classes from element
-//     $(".time-block").removeClass(".present .past .future");
-
-//     // apply new class if task is near/over due date
-//     if (moment().isAfter(hour)) {
-//         $(".time-block").addClass(".past");
-//     } else if (moment().isBefore(time)) {
-//         $(".time-block").addClass(".future");
-//     } else {
-//         $(".time-block").addClass(".present");
-//     }
-// }
+        $(id).addClass('future');
+    }
+};
 
 $('#9amBtn').on('click', function(){
-    var text = $('#9am').val();
+    var text = $('#09').val();
     localStorage.setItem('9am task', text);
 });
 
 $('#10amBtn').on('click', function(){
-    var text = $('#10am').val();
+    var text = $('#10').val();
     localStorage.setItem('10am task', text);
 });
 
 $('#11amBtn').on('click', function(){
-    var text = $('#11am').val();
+    var text = $('#11').val();
     localStorage.setItem('11am task', text);
 });
 
 $('#12pmBtn').on('click', function(){
-    var text = $('#12pm').val();
+    var text = $('#12').val();
     localStorage.setItem('12pm task', text);
 });
 
 $('#1pmBtn').on('click', function(){
-    var text = $('#1pm').val();
+    var text = $('#13').val();
     localStorage.setItem('1pm task', text);
 });
 
 $('#2pmBtn').on('click', function(){
-    var text = $('#2pm').val();
+    var text = $('#14').val();
     localStorage.setItem('2pm task', text);
 });
 
 $('#3pmBtn').on('click', function(){
-    var text = $('#3pm').val();
+    var text = $('#15').val();
     localStorage.setItem('3pm task', text);
 });
 
 $('#4pmBtn').on('click', function(){
-    var text = $('#4pm').val();
+    var text = $('#16').val();
     localStorage.setItem('4pm task', text);
 });
 
 $('#5pmBtn').on('click', function(){
-    var text = $('#5pm').val();
+    var text = $('#17').val();
     localStorage.setItem('5pm task', text);
 });
 
-// checkTime();
-// present();
+paPreFu();
